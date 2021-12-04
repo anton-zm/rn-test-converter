4d7a47163d1655e24842a1a33e744308e4edc985
anton-zm <antonzm34@gmail.com>
2021-12-05 00:22:12 +0300

    layout main page step 1

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
+import { StyleSheet } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 
-const getOptions = (arr: any[]) => {
+const getOptions = (arr: string[]) => {
     return arr.map(e => {
         return {
-            label: '',
-            value: ''
+            label: e,
+            value: e
         }
     })
 }
 
-export const Select = ({data, onChange}:{data: any[], onChange:(v: any) => void}) => {
+export const Select = ({
+        data,
+        styles, 
+        onChange, 
+        value
+    }
+    :
+    {
+        data: string[],
+        styles?: any,
+        onChange:(v: any) => void,
+        value: string
+    }) => {
     return (
         <>
             <RNPickerSelect
                 onValueChange={(value) => onChange(value)}
-                value='football'
+                value={value}
                 placeholder={{}}
                 items={getOptions(data)}
+                style={styles}
             />
         </>
     )
 }
+
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import { Select } from '../components/select';
+import { useStore } from '../store/use-store';
 
 
-export const Main = ({ navigation }: {navigation: any}) => {  
+
+export const Main = ({ navigation }: {navigation: any}) => {
+  const store = useStore()
+  const [from, setFrom] = useState(store.fromCur)  
+  const [to, setTo] = useState(store.toCur)
+
   return (
     <View style={styles.wrapper}>
         <Header title='Конвертер' />
+        <View style={styles.main}>
+          <Text style={styles.date}>Пятница, 12 декабря 2021</Text>
+          <View style={styles.inputs}>
+            <View style={styles.input}>
+              <Select styles={SelectStyles} onChange={setFrom} data={store.currencies} value={from} />
+              <TextInput keyboardType='numeric' style={styles.input_value} />
+            </View>
+            <View style={styles.input}>
+              <Select styles={SelectStyles} onChange={setTo} data={store.currencies} value={to} />
+            </View>
+            
+            
+          </View>
+        </View>
         
-        <Text>Main screen</Text>
+        
         <Footer navigation={navigation} />
     </View>
   );
@@ -19,6 +40,57 @@ const styles = StyleSheet.create({
   wrapper: {
       flex: 1,
       justifyContent: 'space-between',
-
+  },
+  main: {
+    flex:1,
+    paddingHorizontal:10
+  },
+  date: {
+    width: '100%',
+    textAlign: 'center',
+    marginTop: 30,
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontSize: 12,
+    lineHeight: 14,
+    letterSpacing: 0.5,
+    color: '#B2B2B2',
+  },
+  inputs: {
+    marginTop: 48
+  },
+  input: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 30
+  },
+  input_value: {
+    width: 200,
+    height: 55,
+    borderWidth: 2,
+    borderColor: '#d8d1d1',
+    paddingHorizontal: 10
   }
+});
+
+const SelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    backgroundColor: '#d8d1d1',
+    color: 'black',
+    paddingRight: 30,
+  },
+  inputAndroid: {
+    width: 150,
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    backgroundColor: '#d8d1d1',
+    color: 'black',
+    paddingRight: 30,
+    
+  },
 });
